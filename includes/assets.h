a75809776e9c938e24b859fc001ef65846d22743
mailism <domiam09@gmail.com>
2024-03-15 20:16:34 +0700

    solong_gnl ok

--- includes/assets.h
+++ includes/assets.h
@@ -1,12 +1,12 @@
 #ifndef IMAGES_H
 # define IMAGES_H
 
-# define WALL_TILE "./img/brick.xpm"
-# define FLOOR_TILE "./img/floor.xpm"
-# define PLAYER_TILE "./img/player.xpm"
+# define WALL_TILE "./img/1.xpm"
+# define FLOOR_TILE "./img/2.xpm"
+# define PLAYER_TILE "./img/5.xpm"
 # define ENEMY_TILE "./img/ghost.xpm"
-# define COLLECTIBLE_TILE "./img/coin.xpm"
-# define EXIT_TILE "./img/exit.xpm"
+# define COLLECTIBLE_TILE "./img/4.xpm"
+# define EXIT_TILE "./img/3.xpm"
 
 // # define WALL_TOP ""
 // # define WALL_BOTTOM ""
